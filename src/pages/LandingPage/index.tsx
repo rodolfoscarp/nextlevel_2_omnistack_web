6a18f5da12f5ff5b1008c1571a55f0e33112c143
Rodolfo Scarp <rodolfoscarp@gmail.com>
2020-08-05 00:37:58 -0300

    Implementadas rotas

---
+++ src/pages/LandingPage/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {Link} from "react-router-dom"
+import LogoImg from "../../assets/images/logo.svg";
+import LandingImg from "../../assets/images/landing.svg";
+import studyIcon from "../../assets/images/icons/study.svg";
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
+import purpleHeart from '../../assets/images/icons/purple-heart.svg'
+import './styles.css'
+
+const LandingPage = () => {
+  return (
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+        <div className="logo-container">
+          <img src={LogoImg} alt="Proffy" />
+          <h2>Sua plataforma de estudos online.</h2>
+        </div>
+
+        <img
+          src={LandingImg}
+          alt="Plantaforma de Estudos"
+          className="hero-image"
+        />
+
+        <div className="buttons-container">
+          <Link to='/study' className="study">
+            <img src={studyIcon} alt="Estudar" />
+            Estudar
+          </Link>
+          <Link to='/give-classes' className="give-classes">
+            <img src={giveClassesIcon} alt="Estudar" />
+            Dar Aulas
+          </Link>
+        </div>
+
+        <span className="total-connections">
+            Total de 200 conexões realizadas
+            <img src={purpleHeart} alt="Coração Roxo" />
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
